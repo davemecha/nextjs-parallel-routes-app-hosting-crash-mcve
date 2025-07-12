4dc8c312efa94e977e81a2e201899f9924a7472f
Dave Gööck (mechaD) <dave@goeoeck.net>
2025-07-12 02:26:55 +0000

    add workaround

--- next.config.ts
+++ next.config.ts
@@ -5,16 +5,16 @@ import createNextIntlPlugin from 'next-intl/plugin';
  * @type {import('next').NextConfig}
  */
 const nextConfig: NextConfig = {
-  // rewrites: async () => ({
-  //   beforeFiles: [
-  //     {
-  //       source: '/_next/static/chunks/app/:folder*/@:slotName/:path*',
-  //       destination: '/_next/static/chunks/app/:folder*/%40:slotName/:path*',
-  //     },
-  //   ],
-  //   afterFiles: [],
-  //   fallback: [],
-  // }),
+  rewrites: async () => ({
+    beforeFiles: [
+      {
+        source: '/_next/static/chunks/app/:folder*/@:slotName/:path*',
+        destination: '/_next/static/chunks/app/:folder*/%40:slotName/:path*',
+      },
+    ],
+    afterFiles: [],
+    fallback: [],
+  }),
 };
 
 const withNextIntl = createNextIntlPlugin();
